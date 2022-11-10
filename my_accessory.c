5b9462147004c728845ca8f4ddcb5962280990c8
DanN <80033117+dn-test@users.noreply.github.com>
2022-11-10 20:04:51 +0200

    Update my_accessory.c

--- my_accessory.c
+++ my_accessory.c
@@ -13,16 +13,22 @@ void my_accessory_identify(homekit_value_t _value) {
 	printf("accessory identify\n");
 }
 
+homekit_characteristic_t name = HOMEKIT_CHARACTERISTIC_(NAME, "WS2812FX Strip");
+
 homekit_characteristic_t cha_on = HOMEKIT_CHARACTERISTIC_(ON, false);
-homekit_characteristic_t cha_name = HOMEKIT_CHARACTERISTIC_(NAME, "WS2812FX Strip Sony");
 homekit_characteristic_t cha_bright = HOMEKIT_CHARACTERISTIC_(BRIGHTNESS, 50);
 homekit_characteristic_t cha_sat = HOMEKIT_CHARACTERISTIC_(SATURATION, (float) 0);
 homekit_characteristic_t cha_hue = HOMEKIT_CHARACTERISTIC_(HUE, (float) 180);
 
+homekit_characteristic_t fx_on = HOMEKIT_CHARACTERISTIC_(ON, false);
+homekit_characteristic_t fx_bright = HOMEKIT_CHARACTERISTIC_(BRIGHTNESS, 50);
+homekit_characteristic_t fx_sat = HOMEKIT_CHARACTERISTIC_(SATURATION, (float) 0);
+homekit_characteristic_t fx_hue = HOMEKIT_CHARACTERISTIC_(HUE, (float) 180);
+
 homekit_accessory_t *accessories[] = {
     HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_lightbulb, .services=(homekit_service_t*[]) {
         HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
-            HOMEKIT_CHARACTERISTIC(NAME, "WS2812FX Strip Sony"),
+            &name,
             HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Arduino HomeKit"),
             HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "1000001"),
             HOMEKIT_CHARACTERISTIC(MODEL, "ESP8266"),
@@ -31,11 +37,19 @@ homekit_accessory_t *accessories[] = {
             NULL
         }),
         HOMEKIT_SERVICE(LIGHTBULB, .primary=true, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "WS2812FX Strip Control"),
             &cha_on,
-            &cha_name,
             &cha_bright,
             &cha_sat,
             &cha_hue,
+            NULL
+        }),
+        HOMEKIT_SERVICE(LIGHTBULB, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "WS2812FX Strip FX"),
+            &fx_on,
+            &fx_bright,
+            &fx_sat,
+            &fx_hue,
             NULL
         }),
         NULL
